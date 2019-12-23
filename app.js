887c6d27d06d928b36245cd761de5e01bec66f16
Navneet Singh Negi <33743031+navneetsn18@users.noreply.github.com>
2019-12-23 23:24:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,105 @@ window.addEventListener('load',()=>{
     let fc = document.querySelector(".sym");
     let tempsec = document.querySelector(".temp-sec");
     if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(plet tempsec = document.querySelector(".temp-sec");
+let clock = document.querySelector(".clock");
+let date = document.querySelector(".date");
+setInterval(displayclock,500);
+function displayclock(){
+    var time = new Date();
+    var hrs = time.getHours();
+    var min = time.getMinutes();
+    var sec = time.getSeconds();
+    var en = 'AM';
+    var curdate = time.getUTCDate();
+    var month = time.getUTCMonth();
+    var year = time.getFullYear();
+    var day = time.getUTCDay();
+    if(hrs>12){
+        en = 'PM';
+    }
+    if(hrs>12){
+        hrs-=12;
+    }
+    if(hrs==0){
+        hrs=12;
+    }
+    if(hrs<10){
+        hrs = '0'+hrs;
+    }
+    if(min<10){ 
+        min = '0'+min;
+    }
+    if(sec<10){
+        sec = '0'+sec;
+    }
+    if(day == '0'){
+        day = "Sun";
+    }
+    if(day == '1'){
+        day = "Mon";
+    }
+    if(day == '2'){
+        day = "Tue";
+    }
+    if(day == '3'){
+        day = "Wed";
+    }
+    if(day == '4'){
+        day = "Thurs";
+    }
+    if(day == '5'){
+        day = "Fri";
+    }
+    if(day == '6'){
+        day = "Sat";
+    }
+    date.textContent = day + " ," + curdate + "/" + month + "/" + year;
+    clock.textContent = hrs + ":" + min + ":" + sec + " " + en;
+}
+window.addEventListener('load',()=>{
+    let long,lat;
+    let temp = document.querySelector(".temp-degree");
+    let tempdesc = document.querySelector(".temp-desc");
+    let locationplace = document.querySelector(".location-place");
+    let fc = document.querySelector(".sym");
+    if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position=>{
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            let proxy = "https://cors-anywhere.herokuapp.com/";
+            let api = `${proxy}https://api.darksky.net/forecast/211d05599f1d5e0dedaf31b11cde5430/${lat},${long}`;
+            fetch(api)
+                .then(res=>{
+                    return res.json();
+                })
+                .then(data=>{
+                    temp.textContent = data.currently.temperature;
+                    tempdesc.textContent = data.currently.summary;
+                    setIcon(data.currently.icon,document.querySelector(".icon"));
+                    let result = (data.currently.temperature-32)*(5/9);
+                    tempsec.addEventListener("click",()=>{
+                        if(fc.textContent === "F")
+                        {
+                            fc.textContent = "°C";
+                            temp.textContent = Math.floor(result);
+                        }
+                        else
+                        {
+                            fc.textContent = "F";
+                            temp.textContent = data.currently.temperature;
+                        }
+                    })
+                })
+        })
+    }
+    function setIcon(icon,iconID){
+        let skycons = new Skycons({color:"white"});
+        let currenticon = icon.replace(/-/g,"_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID,Skycons[currenticon]);
+    }
+});osition=>{
             long = position.coords.longitude;
             lat = position.coords.latitude;
             let proxy = "https://cors-anywhere.herokuapp.com/";
